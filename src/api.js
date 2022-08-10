0273a1656decd2628dac79a3a52cf28764552ceb
emmanuelolusola <emmanuelolusolaoyeleke@gmail.com>
2022-08-10 09:46:25 +0100

    deployment: weather forecast

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a15e6efff9mshd6ef8679fac039bp15341djsn84c51bb18067",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "f0f14da43fae23d42aa0cdda5dcbf059";
